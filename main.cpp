da6862f4a7d5021beef5e93baab29d5ec70d7e96
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2025-12-11 11:18:54 +0000

    just

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <set>
+
+
+using namespace std;
+
+
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+  int N,C; cin >> N >> C;
+
+  //5 2
+  //21212
+
+  //22211
+
+  //map key-value count를 시키고 value로 정렬 
+
+ 
+
+  return 0;
+}
