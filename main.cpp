a5e4d8248fe43b8750f2e2e31c192f9b833b759b
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2025-12-12 01:54:40 +0000

    //1058

--- main.cpp
+++ main.cpp
@@ -4,28 +4,36 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <vector>
+#include <string>
+
 
 
 using namespace std;
+
+char friends_arr[51][51];
 
 
 
 int main() {
 
+
+    //1058
+
+
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
 
-  int N,C; cin >> N >> C;
+  int N; cin >> N;
 
-  //5 2
-  //21212
 
-  //22211
+  for(int i = 0 ; i<N; ++i){
+    char friends; cin >> friends;
 
-  //map key-value count를 시키고 value로 정렬 
+    friends_arr[i] = friends; //?
+  }
 
- 
 
   return 0;
 }
