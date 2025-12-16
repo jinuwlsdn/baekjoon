986a65db5bcbbced87a673e8f4fe32bfd0d515e2
jinuwlsdn <chlwlsdnaa10@ajou.ac.kr>
2025-12-16 10:52:42 +0000

    1541

--- main.cpp
+++ main.cpp
@@ -12,39 +12,58 @@ using namespace std;
 
 
 
+string s;
 
 
 int main() {
 
 
-  //1439
+  //1541
+
+  
 
 
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  //최소 양수가 있다고 가정하면 뒤에 오는것들은 모두 빼기가 와야한다.
 
+  
+  string num = "";
+  //55
+  //55-50+40 55-(50+40) = -35
+  //sum = 55 -> m = true 55-50 + 니까 num = "" -> 40
+  //sum -= stoi(40);
 
-  string s; cin >> s; 
+  bool m = false;
 
-  int zero =0;
-  int one =0;
+  int sum = 0 ; //결과값
 
-  if(s[0] == '0') zero++;
-  else one ++;
+  cin >> s;
 
-  for(int i = 1; i <s.length() ; ++i){
-    if(s[i] != s[i-1]){
-      if(s[i] == '0') zero++;
-      else one++;
+  for(int i = 0 ; i<=s.size(); ++i){
+    if(s[i] == '+' || s[i] == '-' || s[i] == '\0'){
+      if(m == true){
+        sum -= stoi(num);
+        num = "";
+      }
+      else if(m == false){
+
+        sum += stoi(num);
+        num = "";
+      }
     }
+    else{
+      num += s[i];
+    }
+
+    if(s[i] == '-'){
+      m = true;
+    }
+    //
+
   }
 
-  cout << min(zero,one) << '\n';
   
 
-
-  
-
+  cout << sum <<'\n';
 
   return 0;
 }
